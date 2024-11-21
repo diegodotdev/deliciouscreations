b19b69e44e21e9525cc46fead0c39a0298ddb0d5
Diego <diegohxxiv@gmail.com>
2024-11-21 02:09:20 -0800

    v2

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -11,7 +11,7 @@ import Image from "next/image";
 import { Separator } from "@/components/ui/separator";
 import { Suspense } from "react";
 
-export default function Search() {
+function SearchComponent() {
   const searchParams = useSearchParams();
   const q = searchParams.get("q");
   const { data, isLoading, error } = useQuery({
@@ -22,39 +22,45 @@ export default function Search() {
   if (isLoading) return <Loader />;
   if (error) return <div>error</div>;
   return (
+    <MaxWidthWrapper className="flex flex-col gap-8 py-10">
+      <div className="w-full h-[10vh] flex justify-start items-center">
+        <p className="text-4xl font-[600]">Showing results for: {q}</p>
+      </div>
+      <Separator />
+      <div className="w-full grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-5 place-items-start gap-8">
+        {data?.map((i) => (
+          <Link
+            href={`/recipes/${i.id}`}
+            prefetch={true}
+            className="w-full"
+            key={i.id}
+          >
+            <Card className="w-full">
+              <CardContent className="pt-5">
+                <div className="w-full relative h-[200px] rounded-xl overflow-hidden">
+                  <Image
+                    fill
+                    src={i.imageUrl}
+                    alt={i.name}
+                    className="object-cover"
+                  />
+                </div>
+              </CardContent>
+              <CardFooter>
+                <CardTitle className="text-base">{i.name}</CardTitle>
+              </CardFooter>
+            </Card>
+          </Link>
+        ))}
+      </div>
+    </MaxWidthWrapper>
+  );
+}
+
+export default function Search() {
+  return (
     <Suspense>
-      <MaxWidthWrapper className="flex flex-col gap-8 py-10">
-        <div className="w-full h-[10vh] flex justify-start items-center">
-          <p className="text-4xl font-[600]">Showing results for: {q}</p>
-        </div>
-        <Separator />
-        <div className="w-full grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-5 place-items-start gap-8">
-          {data?.map((i) => (
-            <Link
-              href={`/recipes/${i.id}`}
-              prefetch={true}
-              className="w-full"
-              key={i.id}
-            >
-              <Card className="w-full">
-                <CardContent className="pt-5">
-                  <div className="w-full relative h-[200px] rounded-xl overflow-hidden">
-                    <Image
-                      fill
-                      src={i.imageUrl}
-                      alt={i.name}
-                      className="object-cover"
-                    />
-                  </div>
-                </CardContent>
-                <CardFooter>
-                  <CardTitle className="text-base">{i.name}</CardTitle>
-                </CardFooter>
-              </Card>
-            </Link>
-          ))}
-        </div>
-      </MaxWidthWrapper>
+      <SearchComponent />
     </Suspense>
   );
 }
